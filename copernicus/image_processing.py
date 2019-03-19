19b499164a81f1b8981009f8cfc09137130adb7e
Patrick Merlot <patrick.merlot@computas.com>
2019-03-19 15:41:56 +0100

    Trying out normalization functions to correct automatically for the bad constrast we get after merge. We need artists now in the team!

---
+++ copernicus/image_processing.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+"""
+Image processing.
+
+Image manipulation:
+- https://docs.python-guide.org/scenarios/imaging/#example
+- https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_table_of_contents_imgproc/py_table_of_contents_imgproc.html
+"""
+
+import cv2
+from PIL import Image, ImageFilter, ImageOps
+from scipy.ndimage import imread
+
+
+def split_image_into_RGB(path_to_image):
+    """
+    use OpenCV to split every channel into RGB
+    (https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_core/py_basic_ops/py_basic_ops.html#splitting-and-merging-image-channels),
+    """
+    print(f"Splitting '{path_to_image}' into RGB...")
+    img = cv2.imread(path_to_image)
+    (b, g, r) = cv2.split(img)
+    return (r, g, b)
+
+
+def normalize2(pil_img):
+    return ImageOps.autocontrast(pil_img)
+
+
+def normalize(img):
+  '''
+  Normalize the exposure of an image.
+  @args:
+    {numpy.ndarray} img: an array of image pixels with shape:
+      (height, width)
+  @returns:
+    {numpy.ndarray} an image with shape of `img` wherein
+      all values are normalized such that the min=0 and max=255
+  '''
+  _min = img.min()
+  _max = img.max()
+  return img - _min * 255 / (_max - _min)
+
+
+def convert_jp2_to_png(jp2):
+    """
+    Convert from JPEG2000 to PNG format
+    https://en.wikipedia.org/wiki/JPEG_2000
+    https://en.wikipedia.org/wiki/Extensible_Metadata_Platform
+    https://github.com/DataForGood-Norway/FireFromSpace/blob/master/copernicus/convert_jp2_to_png.sh
+    https://stackoverflow.com/questions/44051852/how-to-read-jpg2000-with-python
+    JPEG2000 for Python: https://softwarerecs.stackexchange.com/questions/51486/jpeg2000-for-python
+    - https://pillow.readthedocs.io/en/4.1.x/handbook/image-file-formats.html#jpeg-2000
+    - https://glymur.readthedocs.io/en/latest/introduction.html
+    """
+    pass
+
+
+def combine_RGB_into_image(r_channel, g_channel, b_channel):
+    """
+    merge this channel back together to mimic the False Color (Urban) filter of Sentinel Playground or other best suited filter.
+    (https://apps.sentinel-hub.com/sentinel-playground/?source=S2&lat=39.80220607474974&lng=-121.51874542236328&zoom=11&preset=3_FALSE_COLOR__URBAN_&layers=B01,B02,B03&maxcc=20&gain=1.0&gamma=1.0&time=2018-05-01%7C2018-11-16&atmFilter=&showDates=false)
+    """
+    return cv2.merge((b_channel, g_channel, r_channel))
+
+
+def get_HSV_from_image():
+    """
+    converted this new image to HSV to extract its hue
+    """
+    pass
--- copernicus/processing/satellite_image.py
+++ copernicus/processing/satellite_image.py
@@ -1,19 +1,41 @@
 # -*- coding: utf-8 -*-
 
 """
-A Flask app to trigger some image processing.
+Satellite Image processing.
+
+Image manipulation:
+- https://docs.python-guide.org/scenarios/imaging/#example
+- https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_table_of_contents_imgproc/py_table_of_contents_imgproc.html
 """
 
-from PIL import Image, ImageFilter
+
+import cv2
+from PIL import Image
 from datetime import datetime
 import numpy as np
+from pathlib import Path, PosixPath
+from datetime import datetime, timedelta
+import image_processing
+
+
 class MonitoredRegion():
     """
     Manage various set of satellite images
     """
-    def __init__(self, path_to_channels):
-        self.path_to_channels = path_to_channels
-        self.image = SatelliteImage(path_to_channels)
+
+    def __init__(self, region_coordinates):
+        self.region_coordinates = None
+        self.path_to_Sentinels_products = None
+        self.images = []
+
+    def query_scihub_copernicus(self):
+        """
+        https://scihub.copernicus.eu/
+        https://scihub.copernicus.eu/twiki/do/view/SciHubWebPortal/APIHubDescription
+        https://scihub.copernicus.eu/userguide/BatchScripting#Query_via_wget
+        """
+        path_to_Sentinels_products = None
+        return path_to_Sentinels_products
 
 
 class SatelliteImage():
@@ -24,50 +46,73 @@ class SatelliteImage():
     - fires detected on it, or not
     - store the image if relevant, remove it otherwise
     """
+
     def __init__(self, path_to_channels: str):
-        self.sentinel_metadata = {} # coordinates, resolution, cloud coverage, ...
-        self.channels = {} # raw and RGB for each channel
-        # {"path_to_file": x for x in path_to_channels.glob("*.png") if x.is_file()}
+        # coordinates, resolution, cloud coverage, ...
+        self.channels = {}
+        for pth in path_to_channels.glob("*.png"):
+            if pth.is_file():
+                (tile_name, dtime, channel) = str(pth.resolve()).split(".")[0].split("_")
+                obj = {
+                    "path": pth,
+                    "tile_name": tile_name,
+                    "datetime_str": dtime,
+                    "datetime": datetime.strptime(dtime, '%Y%m%dT%H%M%S'),
+                    "RGB": None}
+                self.channels[channel] = obj
+        self.sentinel_metadata = {}
+        self.coordinates = None
 
-    def get_channels(self):
-        """
-        Get information about the various channels from Sentinel's zip/folder
-        """
-        pass
+    def split_channels_into_RGBs(self):
+        for ch in self.channels:
+            self.channels[ch]["RGB"] = image_processing.split_image_into_RGB(str(self.channels[ch]["path"].resolve()))
 
+    def filter_image(self, filter_name):
+        if filter_name == "FALSE_COLOR_URBAN":
+            min_common_size = min([self.channels[ch]["RGB"][0].shape[0]
+                                   for ch in ["B04", "B11", "B12"]])
+            r_step = int(self.channels["B12"]["RGB"][0].shape[0] / min_common_size)
+            r_intensity = self.channels["B12"]["RGB"][0][::r_step, ::r_step]
+            g_step = int(self.channels["B11"]["RGB"][0].shape[0] / min_common_size)
+            g_intensity = self.channels["B11"]["RGB"][1][::g_step, ::g_step]
+            b_step = int(self.channels["B04"]["RGB"][0].shape[0] / min_common_size)
+            b_intensity = self.channels["B04"]["RGB"][2][::b_step, ::b_step]
+            # return cv2.merge((b_intensity, g_intensity, r_intensity))
+            return cv2.merge((r_intensity, g_intensity, b_intensity))
+        else:
+            raise AttributeError(f"The filter name '{filter_name}' isn't recognized! Consider implementing it!")
 
     def locate_fire_from_image(self):
         """
         Returns coordinates of fires detected in the satellite image.
+        https://realpython.com/python-opencv-color-spaces/#color-spaces-and-reading-images-in-opencv
+        http://scikit-image.org/docs/stable/auto_examples/features_detection/plot_blob.html#sphx-glr-auto-examples-features-detection-plot-blob-py
+        http://scikit-image.org/docs/stable/auto_examples/color_exposure/plot_ihc_color_separation.html#sphx-glr-auto-examples-color-exposure-plot-ihc-color-separation-py
+        http://scikit-image.org/docs/stable/auto_examples/edges/plot_canny.html#sphx-glr-auto-examples-edges-plot-canny-py
+        http://scikit-image.org/docs/stable/auto_examples/segmentation/plot_random_walker_segmentation.html#sphx-glr-auto-examples-segmentation-plot-random-walker-segmentation-py
+        http://scikit-image.org/docs/stable/auto_examples/color_exposure/plot_adapt_rgb.html#sphx-glr-auto-examples-color-exposure-plot-adapt-rgb-py
         """
         return "well... coming soon ;)"
 
 
-    def convert_jp2_to_png(self):
-        """
-        Convert from JPEG2000 to PNG format
-        https://github.com/DataForGood-Norway/FireFromSpace/blob/master/copernicus/convert_jp2_to_png.sh
-        """
-        pass
+
+def main():
+    region_coordinates = []
+    region = MonitoredRegion(region_coordinates)
+    path_to_Sentinels_products = region.query_scihub_copernicus()
+
+    path_to_channels = Path("..") / "data"
+    sat_image = SatelliteImage(path_to_channels)
+    sat_image.split_channels_into_RGBs()
+    img = sat_image.filter_image("FALSE_COLOR_URBAN")
+    import matplotlib.pyplot as plt
+    plt.imshow(img); plt.show() # showing smoke in red, but too dark if not 'img*5'
+    
+    # normalize the image using Pillow
+    pil_im = Image.fromarray(img.astype("uint8"), "RGB")
+    plt.imshow(image_processing.normalize2(pil_im)); plt.show() # showing smoke in red
+    from IPython import embed; embed()
 
 
-    def split_channels_into_RGBs(self, channels_images):
-        """
-        use OpenCV to split every channel into RGB
-        (https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_core/py_basic_ops/py_basic_ops.html#splitting-and-merging-image-channels),
-        """
-        pass
-
-
-    def combine_RGB_into_image(self, R_channel, G_channel, B_channel) -> Image:
-        """
-        merge this channel back together to mimic the False Color (Urban) filter of Sentinel Playground
-        (https://apps.sentinel-hub.com/sentinel-playground/?source=S2&lat=39.80220607474974&lng=-121.51874542236328&zoom=11&preset=3_FALSE_COLOR__URBAN_&layers=B01,B02,B03&maxcc=20&gain=1.0&gamma=1.0&time=2018-05-01%7C2018-11-16&atmFilter=&showDates=false)
-        """
-        pass
-
-
-    def get_HSV_from_image(self):
-        """
-        converted this new image to HSV to extract its hue
-        """
+if __name__ == '__main__':
+    main()
